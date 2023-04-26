84a1e42ced08d8d9e6bdd9683661568d523fc364
Luka <102422999+luka43@users.noreply.github.com>
2023-04-26 23:54:16 +0200

    rmetamask-scripts.js
    renamed

---
+++ scripts/metamask-scripts.js
@@ -0,0 +1,29 @@
+let accountAddress;
+document.getElementById("connect-wallet").addEventListener("click", event => {
+    let button = event.target;
+    ethereum.request({method: "eth_requestAccounts"}).then(accounts => {
+        accountAddress = accounts[0];
+
+        ethereum.request({method: "eth_getBalance", params: [accountAddress, "latest"]}).then(result =>{
+            let wei = parseInt(result,16);
+            let balance = wei / (10**18);
+            button.innerHTML = accountAddress;
+            document.getElementById("balance").innerHTML = balance.toFixed(4);
+            console.log(balance + "ETH");
+        })
+    })
+})
+document.getElementById("donate-button").addEventListener("click", () => {
+    params = [{
+          from: accountAddress,
+          to: '0x95686ebdE122E369c12409ECFA94ab08b0610246',
+          value: '0xAA87BEE538000',
+        }];
+
+    ethereum.request({method: 'eth_sendTransaction', params,}).then((result) => {
+        console.log(result)
+})
+        .catch((error) => {
+            console.log(error)
+        });
+})
