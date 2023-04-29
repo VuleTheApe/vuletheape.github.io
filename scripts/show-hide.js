8a83702a5cf175108e178551af31eb765f58d9f1
Luka <102422999+luka43@users.noreply.github.com>
2023-04-29 04:55:09 +0200

    Dropdown animation on list

--- scripts/show-hide.js
+++ scripts/show-hide.js
@@ -1,22 +1,23 @@
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
+document.getElementById("python-item-1").style.maxHeight = "500px";
+document.getElementById("webdev-item-1").style.maxHeight = "500px";
+document.getElementById("webdev-item-2").style.maxHeight = "500px";
+document.getElementById("webdev-item-3").style.maxHeight = "500px";
+document.getElementById("webdev-item-4").style.maxHeight = "500px";
 
 function pythonList() {
     var pythonItem1 = document.getElementById("python-item-1");
     var dropdownIcon = document.getElementById("python-plus-ico");
 
-    if (pythonItem1.style.display === "none") {
-        pythonItem1.style.display = "flex";
+    if (pythonItem1.style.maxHeight === "0px") {
+        console.log(pythonItem1.style.maxHeight);
+        pythonItem1.style.maxHeight = "500px";
         dropdownIcon.src = "thumbs/minus.png";
     }
-    else if (pythonItem1.style.display === "flex"){
-        pythonItem1.style.display = "none"
+    else if (pythonItem1.style.maxHeight === "500px"){
+        console.log(pythonItem1.style.maxHeight);
+        pythonItem1.style.maxHeight = "0px";
         dropdownIcon.src = "thumbs/plus.png";
+
     }
   }
 
@@ -27,31 +28,31 @@ function webdevelopmentList() {
     var webdevItem3 = document.getElementById("webdev-item-3");
     var webdevItem4 = document.getElementById("webdev-item-4");
 
-    if (webdevItem1.style.display === "none") {
-        webdevItem1.style.display = "flex";
+    if (webdevItem1.style.maxHeight === "0px") {
+        webdevItem1.style.maxHeight = "500px";
         dropdownIcon.src = "thumbs/minus.png";
 
     }
-    else if (webdevItem1.style.display === "flex"){
-        webdevItem1.style.display = "none";
+    else if (webdevItem1.style.maxHeight === "500px"){
+        webdevItem1.style.maxHeight = "0px";
         dropdownIcon.src = "thumbs/plus.png";
     }
-    if (webdevItem2.style.display === "none") {
-        webdevItem2.style.display = "flex";
+    if (webdevItem2.style.maxHeight === "0px") {
+        webdevItem2.style.maxHeight = "500px";
     }
-    else if (webdevItem2.style.display === "flex"){
-        webdevItem2.style.display = "none";
+    else if (webdevItem2.style.maxHeight === "500px"){
+        webdevItem2.style.maxHeight = "0px";
     }
-    if (webdevItem3.style.display === "none") {
-        webdevItem3.style.display = "flex";
+    if (webdevItem3.style.maxHeight === "0px") {
+        webdevItem3.style.maxHeight = "500px";
     }
-    else if (webdevItem3.style.display === "flex"){
-        webdevItem3.style.display = "none";
+    else if (webdevItem3.style.maxHeight === "500px"){
+        webdevItem3.style.maxHeight = "0px";
     }
-    if (webdevItem4.style.display === "none") {
-        webdevItem4.style.display = "flex";
+    if (webdevItem4.style.maxHeight === "0px") {
+        webdevItem4.style.maxHeight = "500px";
     }
-    else if (webdevItem4.style.display === "flex"){
-        webdevItem4.style.display = "none";
+    else if (webdevItem4.style.maxHeight === "500px"){
+        webdevItem4.style.maxHeight = "0px";
     }
   }
