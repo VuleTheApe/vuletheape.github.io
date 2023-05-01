ecde33d6cc1ad0a236591789454ca80049b55dde
luka43 <102422999+luka43@users.noreply.github.com>
2023-05-01 17:44:35 +0200

    updated metamask script

--- js/metamask-scripts.js
+++ js/metamask-scripts.js
@@ -1,29 +1,14 @@
-var accountAddress;
-var connected = 0;
-var balance = 0;
+window.onload = () => {
+    checkMetamask();
+ };
 
 document.getElementById("wallet-connect").addEventListener("click", () => {
-    if (connected == 0){
 
-        ethereum.request({method: "eth_requestAccounts"}).then(accounts => {
-            accountAddress = accounts[0];
-
-            ethereum.request({method: "eth_getBalance", params: [accountAddress, "latest"]}).then(result =>{
-                let wei = parseInt(result,16);
-                balance = wei / (10**18);
-                document.getElementById("wallet-balance").innerHTML = balance.toFixed(5);
-
-            });
-
-        connected = 1;
-        document.getElementById("wallet-balance").style.visibility = "visible";
-        document.getElementById("wallet-connect").title="Copy to clipboard";
-        document.getElementById("button-text").innerHTML = accountAddress;
-        document.getElementById("copy-icon").style.display = "block";
-        })
-    }
-    else if (connected == 1) {
-        navigator.clipboard.writeText(accountAddress);
+    if (accounts.length) {
+        navigator.clipboard.writeText(accounts[0]);
+    } else {
+        console.log(accounts.length)
+        connectMetamask();
     }
 });
 
@@ -41,3 +26,34 @@ document.getElementById("donate-button").addEventListener("click", () => {
             console.log(error)
         });
 });
+
+async function checkMetamask() {
+    accounts = await ethereum.request({method: 'eth_accounts'});
+    balance =  parseInt(await ethereum.request({method: "eth_getBalance", params: [accounts[0], "latest"]}), 16);
+    balance = balance / (10**18);
+
+    if (accounts.length) {
+        account = accounts[0].substring(0, 6) + ".." + accounts[0].substring(accounts[0].length - 6);
+        updateButtonText();
+    } else {
+    console.log("Metamask is not connected");
+    }
+}
+
+async function connectMetamask() {
+    accounts = await ethereum.request({method: "eth_requestAccounts"});
+    balance_result = await ethereum.request({method: "eth_getBalance", params: [accounts[0], "latest"]});
+    let wei = parseInt(balance_result,16);
+    balance = wei / (10**18);
+    updateButtonText();
+
+}
+
+function updateButtonText() {
+    account = accounts[0].substring(0, 6) + ".." + accounts[0].substring(accounts[0].length - 6);
+    document.getElementById("wallet-balance").innerHTML = balance.toFixed(5);
+    document.getElementById("wallet-balance").style.visibility = "visible";
+    document.getElementById("button-text").innerHTML = account;
+    document.getElementById("copy-icon").style.display = "block";
+    document.getElementById("wallet-connect").title="Copy to clipboard";
+}
